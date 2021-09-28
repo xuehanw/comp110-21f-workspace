afdd689d5fa47e479c83a4da59304fb1f3f97b5c
Your Namexuehanw <xuehanw@email.unc.edu>
2021-09-28 14:52:06 -0400

    commit 3

--- lessons/sum_test.py
+++ lessons/sum_test.py
@@ -15,9 +15,9 @@ def test_sum_single_item() -> None:
 
 
 def test_sum_many_items() -> None:
-    xs: list[float] = [1.0,2.0,3.0]
+    xs: list[float] = [1.0, 2.0, 3.0]
     assert sum(xs) == 6.0
 
 
 def test_sum_many_items_again() -> None:
-    assert sum([-1.0,1.0,-2.0,2.0]) == 0
+    assert sum([-1.0, 1.0, -2.0, 2.0]) == 0
